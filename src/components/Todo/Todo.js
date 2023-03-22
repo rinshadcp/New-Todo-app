e06fca55fe048b613fa8ea4aa9c3170b1044890d
rinshadcp <rinshadcp7337@gmail.com>
2023-03-22 14:04:16 +0530

    fourth commit

--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -1,10 +1,14 @@
 import React, { useState, useRef, useEffect } from "react";
 import "./Todo.css";
+import { MdFileDownloadDone } from "react-icons/md";
+import { MdEditNote } from "react-icons/md";
+import { MdDeleteSweep } from "react-icons/md";
+
 function Todo() {
   const [todo, setTodo] = useState("");
   const [todos, setTodos] = useState([]);
   const addTodo = () => {
-    setTodos([...todos, todo]);
+    setTodos([...todos, { list: todo, id: Date.now() }]);
     setTodo("");
   };
   const submitHandle = (event) => {
@@ -14,6 +18,18 @@ function Todo() {
   useEffect(() => {
     inputRef.current.focus();
   });
+  const deleteItem = (id) => {
+    setTodos(todos.filter((to) => to.id !== id));
+  };
+  const onComplete = (id) => {
+    let complete = todos.map((todo) => {
+      if (todo.id === id) {
+        return { ...todo, status: !todo.status };
+      }
+      return todo;
+    });
+    setTodos(complete);
+  };
 
   return (
     <div className="container">
@@ -34,7 +50,33 @@ function Todo() {
       <div className="list">
         <ul>
           {todos.map((to) => (
-            <li key={todos.index}>{to}</li>
+            <li key={to.id} className="list-items">
+              <div
+                className="list-items-list"
+                id={to.status ? "list-item" : ""}
+              >
+                {to.list}
+              </div>
+              <span>
+                <MdFileDownloadDone
+                  className="list-items-icons"
+                  id="done"
+                  title="Complete"
+                  onClick={() => onComplete(to.id)}
+                />
+                <MdEditNote
+                  className="list-items-icons"
+                  id="edit"
+                  title="Edit"
+                />
+                <MdDeleteSweep
+                  className="list-items-icons"
+                  id="delete"
+                  title="Delete"
+                  onClick={() => deleteItem(to.id)}
+                />
+              </span>
+            </li>
           ))}
         </ul>
       </div>
