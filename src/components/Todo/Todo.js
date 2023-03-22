d4eadecc7f4f650bbc216d2aac273a5ae647ef78
rinshadcp <rinshadcp7337@gmail.com>
2023-03-22 12:02:48 +0530

    third commit

--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -1,9 +1,45 @@
-import React from 'react'
-
+import React, { useState, useRef, useEffect } from "react";
+import "./Todo.css";
 function Todo() {
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+  const addTodo = () => {
+    setTodos([...todos, todo]);
+    setTodo("");
+  };
+  const submitHandle = (event) => {
+    event.preventDefault();
+  };
+  const inputRef = useRef("null");
+  useEffect(() => {
+    inputRef.current.focus();
+  });
+
   return (
-    <div>Hello world!!!</div>
-  )
+    <div className="container">
+      <h2>TODO APP</h2>
+      <form className="form-group" onSubmit={submitHandle}>
+        <input
+          type="text"
+          value={todo}
+          ref={inputRef}
+          placeholder="Enter your tasks"
+          className="form-control"
+          onChange={(event) => setTodo(event.target.value)}
+        />
+
+        <button onClick={addTodo}>ADD</button>
+      </form>
+
+      <div className="list">
+        <ul>
+          {todos.map((to) => (
+            <li key={todos.index}>{to}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
 
-export default Todo
+export default Todo;
